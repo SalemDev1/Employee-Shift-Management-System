6ebbb4a210fd09e75ca56ebcd8bf8493a3236cec
Mamun Salem <salemtwin2@gmail.com>
2024-09-21 16:37:32 -0400

    Initialize employees with shift schedules

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -8,27 +8,3 @@ const employees = [
     { name: 'Ibrahim', shifts: [{ day: 'Friday', hours: 8 }] }
 ];
 
-//Task 2- Create a Function to Display Employee Shift Details
-function displayEmployeeShifts(employee) {
-    console.log(`Employee: ${employee.name}`);
-    employee.shifts.forEach(shift => {console.log(`Day: ${shift.day}, Hours: ${shift.hours}`);
-    });
-}
-
-//Task 3- Create a Function to Assign a New Shift
-function assignShift(employeeName, newDay, newHours) {
-    let employee = employees.find(worker => worker.name === employeeName);
-    
-    if (employee) {
-        let alreadyHasShift = employee.shifts.some(shift => shift.day === newDay);
-
-        if (alreadyHasShift) {
-            console.log(`Hey, looks like ${employeeName} is already working on ${newDay}.`);
-        } else {
-            employee.shifts.push({ day: newDay, hours: newHours });
-            console.log(`Great! ${employeeName} is now scheduled to work on ${newDay} for ${newHours} hours.`);
-        }
-    } else {
-        console.log(`Oops, couldn't find an employee named ${employeeName}. `);
-    }
-}
