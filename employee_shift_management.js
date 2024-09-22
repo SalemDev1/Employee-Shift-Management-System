03f3fcf50fc5c0023746f56721eb751e0535d2d2
Mamun Salem <salemtwin2@gmail.com>
2024-09-21 20:20:20 -0400

    Create calculateTotalHours function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -35,8 +35,28 @@ function assignShift(workerName, newDay, newHours) {
             console.log("Shift assigned to " + workerName + " on " + newDay + " for " + newHours + " hours.");
         }
     } else {
-        console.log(" We couldn't locate " + workerName + " in our system.");
+        console.log(" We couldn't locate " + workerName + " in our system, but its okay because we should give him the weekend off");
     }
 }
 assignShift('Leala', 'Friday', 8);
+assignShift('Markum Reed', 'Saturday', 23 )
 
+//Task 4- Create a Function to Calculate Total Hours Worked
+function calculateTotalHours(workerName) {
+    let worker = employees.find(function(worker) {
+        return worker.name === workerName;
+    });
+
+    if (worker) {
+        let totalHours = 0;
+        worker.shifts.forEach(function(shift) {
+            totalHours += shift.hours;
+        });
+        console.log(workerName + " has worked a total of " + totalHours + " hours this week.");
+    } else {
+        console.log("Couldn't find " + workerName + " in our system.");
+    }
+}
+
+// Calculate total hours worked for Leala
+calculateTotalHours('Leala');
